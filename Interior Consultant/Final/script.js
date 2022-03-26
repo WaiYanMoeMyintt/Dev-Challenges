453fd4684484339ef9c1cd445b23113f82c4fef5
WaiYanMoeMyint <87901620+WaiYanMoeMyintt@users.noreply.github.com>
2022-03-26 17:45:59 +0630

    Update a complete javascript code

--- Interior Consultant/Final/script.js
+++ Interior Consultant/Final/script.js
@@ -1,1 +1,7 @@
-window.console.log("Hi");
+const menuItems = document.querySelector(".navbar-links");
+const menuBars =document.querySelector(".humburger");
+
+menuBars.addEventListener("click",()=>{
+     menuItems.classList.toggle("active");
+     menuBars.classList.toggle("active");
+})
