fb204aebfc3a00d58475b550a7ec5e9601564a2e
WaiYanMoeMyint <87901620+WaiYanMoeMyintt@users.noreply.github.com>
2022-04-09 21:28:14 +0630

    Update a complete js file

--- Edie Home Page/Final/script.js
+++ Edie Home Page/Final/script.js
@@ -1,1 +1,8 @@
-console.log
+//for menu bars
+const menuBars = document.querySelector("#navbar-menu");
+const navLinks = document.querySelector("#nav-links");
+
+menuBars.addEventListener("click",()=>{
+     navLinks.classList.toggle("show");
+     // menuBars.classList.toggle("active");
+})
